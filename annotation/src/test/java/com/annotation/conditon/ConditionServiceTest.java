4e9c77af7942b92f1b006d9e487fb174d3b3f239
golf <491550934@qq.com>
2022-01-19 23:51:12 +0800

    feat: 父工程中引入Junit包

--- annotation/src/test/java/com/annotation/conditon/ConditionServiceTest.java
+++ annotation/src/test/java/com/annotation/conditon/ConditionServiceTest.java
@@ -1,11 +1,15 @@
 package com.annotation.conditon;
 
 import com.annotation.conditon.pojo.ConditionalOnBeanTest;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class ConditionServiceTest {
     @Autowired
     private ConditionService conditionService;
@@ -14,7 +18,7 @@ class ConditionServiceTest {
     @Test
     void getOs() {
         Os os = conditionService.getOs();
-        assert os.getName().equals("Windows");
+        Assert.assertEquals("Windows", os.getName());
     }
 
     @Test
