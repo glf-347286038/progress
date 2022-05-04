97e2bc35d03999bb47684984e2e00c681136165e
golf <491550934@qq.com>
2022-05-05 01:11:09 +0800

    feat: 消费消息时异常的处理和重试机制

--- rabbitmq/src/main/java/com/rabbitmq/direct/DirectRabbitMqConfig.java
+++ rabbitmq/src/main/java/com/rabbitmq/direct/DirectRabbitMqConfig.java
@@ -5,6 +5,9 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.retry.MessageRecoverer;
+import org.springframework.amqp.rabbit.retry.RepublishMessageRecoverer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -40,5 +43,36 @@ public class DirectRabbitMqConfig {
     @Bean
     public Binding bindingDirectSms() {
         return BindingBuilder.bind(smsDirectQueue()).to(directExchange()).with("sms");
+    }
+
+    /**
+     * 定义一个专门存放错误消息的队列
+     *
+     * @return 队列
+     */
+    @Bean
+    public Queue directErrorQueue() {
+        return new Queue("direct.error.queue");
+    }
+
+    @Bean
+    public DirectExchange directErrorExchange() {
+        return new DirectExchange("direct.error.exchange");
+    }
+
+    @Bean
+    public Binding bindingErrorQueue() {
+        return BindingBuilder.bind(directErrorQueue()).to(directErrorExchange()).with("error");
+    }
+
+    /**
+     * 消费消息时出现异常且重试次数超过配置的次数会将消息保存到指定的队列中
+     *
+     * @param rabbitTemplate 消息队列模板
+     * @return 消息恢复器
+     */
+    @Bean
+    public MessageRecoverer messageRecoverer(RabbitTemplate rabbitTemplate) {
+        return new RepublishMessageRecoverer(rabbitTemplate, "direct.error.exchange", "error");
     }
 }
--- rabbitmq/src/main/java/com/rabbitmq/direct/consumer/SmsDirectConsumer.java
+++ rabbitmq/src/main/java/com/rabbitmq/direct/consumer/SmsDirectConsumer.java
@@ -1,9 +1,9 @@
 package com.rabbitmq.direct.consumer;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.ExchangeTypes;
 import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 /**
  * @Author: golf
@@ -12,14 +12,12 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @RabbitListener(bindings = @QueueBinding(value = @Queue(value = "direct.sms.queue"),
-        exchange = @Exchange(value = "direct_test_exchange", type = ExchangeTypes.DIRECT)))
+        exchange = @Exchange(value = "direct_test_exchange")))
 public class SmsDirectConsumer {
     @RabbitHandler
-    public void smsDirectConsumer(String message) throws Exception {
+    public void smsDirectConsumer(String message) {
         // 消费端出错，broker会一直重试，如果不做限制会出现死循环
-//        if (true) {
-//            throw new Exception("故意报错");
-//        }
         log.info("smsDirectConsumer接收到消息:{},开始处理消息", message);
+        Assert.isTrue(message.length() < 3, "消息长度小于3,消费者报错");
     }
 }
