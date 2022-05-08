ceb74d6d500eb547206555c4356f6f240039fb2a
golf <491550934@qq.com>
2022-05-09 00:40:58 +0800

    feat: 匿名内部类带参数

--- base/src/main/java/com/base/se/inner/InnerClass.java
+++ base/src/main/java/com/base/se/inner/InnerClass.java
@@ -14,17 +14,17 @@ public class InnerClass {
         // 匿名内部类
         Interface01 interface01 = new Interface01() {
             @Override
-            public String show() {
-                return "使用了匿名内部类";
+            public String show(int x, String a) {
+                return "使用了匿名内部类" + x + a;
             }
         };
-        interface01.show();
+        log.info(interface01.show(1, "golf"));
 
-        Interface01 interface012 = () -> {
+        Interface01 interface012 = (x, a) -> {
             log.info("study");
-            return "使用lambda测试匿名内部类";
+            return "使用lambda测试匿名内部类" + x + a;
         };
-        log.info(interface012.show());
+        log.info(interface012.show(2, "golf"));
     }
 
     public void out() {
--- base/src/main/java/com/base/se/inner/Interface01.java
+++ base/src/main/java/com/base/se/inner/Interface01.java
@@ -10,5 +10,5 @@ public interface Interface01 {
      *
      * @return ""
      */
-    String show();
+    String show(int a, String b);
 }
