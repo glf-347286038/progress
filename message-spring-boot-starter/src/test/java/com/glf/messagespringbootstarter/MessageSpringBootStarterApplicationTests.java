3169c196f6279316e3bcfbd896773246489f5cbe
golf <491550934@qq.com>
2022-10-13 20:42:15 +0800

    feat: 自定义Spring Boot starter

---
+++ message-spring-boot-starter/src/test/java/com/glf/messagespringbootstarter/MessageSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glf.messagespringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
