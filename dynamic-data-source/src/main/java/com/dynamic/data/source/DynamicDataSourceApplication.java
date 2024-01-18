fa89f2c84c0e2ea0dab3cb216d6e0bac375c9eca
golf <491550934@qq.com>
2024-01-18 19:57:11 +0800

    feat:创建动态数据源模块

---
+++ dynamic-data-source/src/main/java/com/dynamic/data/source/DynamicDataSourceApplication.java
@@ -0,0 +1,15 @@
+package com.dynamic.data.source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author golf
+ * @date 2024/1/18 19:28
+ */
+@SpringBootApplication
+public class DynamicDataSourceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicDataSourceApplication.class, args);
+    }
+}
