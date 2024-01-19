976fbb7e1b91f952997390739250dc6675338dc8
golf <491550934@qq.com>
2024-01-19 14:11:35 +0800

    feat:引入mybatis-plus

---
+++ dynamic-data-source/src/main/java/com/dynamic/data/source/module/user/controller/SysUserController.java
@@ -0,0 +1,26 @@
+package com.dynamic.data.source.module.user.controller;
+
+import com.dynamic.data.source.module.user.domain.SysUser;
+import com.dynamic.data.source.module.user.service.SysUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author golf
+ * @date 2024/1/19 13:59
+ */
+@RequiredArgsConstructor
+@RequestMapping("/sysUser")
+@RestController
+public class SysUserController {
+    private final SysUserService sysUserService;
+
+    @PostMapping("/add")
+    public void add(@RequestBody SysUser user) {
+        sysUserService.save(user);
+    }
+
+}
